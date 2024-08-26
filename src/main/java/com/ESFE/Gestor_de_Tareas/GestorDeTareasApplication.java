fae96b3123687223ad53afdf31342822213259fd
gabri777Des <gabrisac2006@gmail.com>
2024-08-25 21:53:39 -0600

    Primer sprint 50%

---
+++ src/main/java/com/ESFE/Gestor_de_Tareas/GestorDeTareasApplication.java
@@ -0,0 +1,13 @@
+package com.ESFE.Gestor_de_Tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ESFE/Gestor_de_Tareas/GestorDeTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ESFE.Gestor_de_Tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
